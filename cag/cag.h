98d8d4d261cd3cb774eecb986c72147586ad0925
Andrey Gramakov <mail@agramakov.me>
2018-10-09 17:01:28 +0200

    init commit

---
+++ cag/cag.h
@@ -0,0 +1,8 @@
+//
+// Created by dongr on 09/10/2018.
+//
+
+#ifndef CAG_CAG_H
+#define CAG_CAG_H
+
+#endif //CAG_CAG_H
---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by dongr on 09/10/2018.
+//
+
+#include <iostream>
+#include "cag/cag.h"
+
+
+int main()
+{
+    std::cout<<"Test"<<std::endl;
+    return 0;
+}
